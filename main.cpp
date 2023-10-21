dc4359e154bdba70ec584cf08ddb6461c8992bc6
Ilya Baretkin <usaasu386@mail.ru>
2023-10-21 21:38:37 +0700

    Changed ANSI to UTF-8

--- main.cpp
+++ main.cpp
@@ -12,45 +12,45 @@ int main() {
 	Publisher publisher_static = {}, * publisher_dynamic = {};
 	Book books_static[4], * books_dynamic = (Book*)calloc(4, sizeof(Book));
 	Magazine magazines_static[4], * magazines_dynamic = (Magazine*)calloc(4, sizeof(Magazine));
-	char title[72] = "Война и мир", author_last_name[22] = "Толстой", genre_name[22] = "Роман", publisher_name[32] = "АСТ";
-	// Работа со статической структурой автора
+	char title[72] = "Р’РѕР№РЅР° Рё РјРёСЂ", author_last_name[22] = "РўРѕР»СЃС‚РѕР№", genre_name[22] = "Р РѕРјР°РЅ", publisher_name[32] = "РђРЎРў";
+	// Р Р°Р±РѕС‚Р° СЃРѕ СЃС‚Р°С‚РёС‡РµСЃРєРѕР№ СЃС‚СЂСѓРєС‚СѓСЂРѕР№ Р°РІС‚РѕСЂР°
 	createAuthor(&author_static);
 	printAuthorInfo(author_static);
-	// Работа с динамической структурой автора
+	// Р Р°Р±РѕС‚Р° СЃ РґРёРЅР°РјРёС‡РµСЃРєРѕР№ СЃС‚СЂСѓРєС‚СѓСЂРѕР№ Р°РІС‚РѕСЂР°
 	createAuthor(author_dynamic);
 	printAuthorInfo(*author_dynamic);
-	// Работа со статической структурой жанра
+	// Р Р°Р±РѕС‚Р° СЃРѕ СЃС‚Р°С‚РёС‡РµСЃРєРѕР№ СЃС‚СЂСѓРєС‚СѓСЂРѕР№ Р¶Р°РЅСЂР°
 	createGenre(&genre_static);
 	printGenreInfo(genre_static);
-	// Работа с динамической структурой жанра
+	// Р Р°Р±РѕС‚Р° СЃ РґРёРЅР°РјРёС‡РµСЃРєРѕР№ СЃС‚СЂСѓРєС‚СѓСЂРѕР№ Р¶Р°РЅСЂР°
 	createGenre(genre_dynamic);
 	printGenreInfo(*genre_dynamic);
-	// Работа со статической структурой издателя
+	// Р Р°Р±РѕС‚Р° СЃРѕ СЃС‚Р°С‚РёС‡РµСЃРєРѕР№ СЃС‚СЂСѓРєС‚СѓСЂРѕР№ РёР·РґР°С‚РµР»СЏ
 	createPublisher(&publisher_static);
 	printPublisherInfo(publisher_static);
-	// Работа с динамической структурой издателя
+	// Р Р°Р±РѕС‚Р° СЃ РґРёРЅР°РјРёС‡РµСЃРєРѕР№ СЃС‚СЂСѓРєС‚СѓСЂРѕР№ РёР·РґР°С‚РµР»СЏ
 	createPublisher(publisher_dynamic);
 	printPublisherInfo(*publisher_dynamic);
-	// Работа со статическим массивом структур книг
+	// Р Р°Р±РѕС‚Р° СЃРѕ СЃС‚Р°С‚РёС‡РµСЃРєРёРј РјР°СЃСЃРёРІРѕРј СЃС‚СЂСѓРєС‚СѓСЂ РєРЅРёРі
 	for (int i = 0; i < 4; i++) {
 		createBook(&(books_static[i]));
 	}
 	findBooksByTitleAndAuthor(books_static, 4, title, author_last_name);
 	findBooksByAuthor(books_static, 4, author_last_name);
 	findBooksByGenre(books_static, 4, genre_name);
-	// Работа с динамическим массивом структур книг
+	// Р Р°Р±РѕС‚Р° СЃ РґРёРЅР°РјРёС‡РµСЃРєРёРј РјР°СЃСЃРёРІРѕРј СЃС‚СЂСѓРєС‚СѓСЂ РєРЅРёРі
 	for (int i = 0; i < 4; i++) {
 		createBook(books_dynamic + i);
 	}
 	findBooksByTitleAndAuthor(books_dynamic, 4, title, author_last_name);
 	findBooksByAuthor(books_dynamic, 4, author_last_name);
 	findBooksByGenre(books_dynamic, 4, genre_name);
-	// Работа со статическим массивом структур журналов
+	// Р Р°Р±РѕС‚Р° СЃРѕ СЃС‚Р°С‚РёС‡РµСЃРєРёРј РјР°СЃСЃРёРІРѕРј СЃС‚СЂСѓРєС‚СѓСЂ Р¶СѓСЂРЅР°Р»РѕРІ
 	for (int i = 0; i < 4; i++) {
 		createMagazine(&(magazines_static[i]));
 	}
 	findMagazinesByPublisher(magazines_static, 4, publisher_name);
-	// Работа с динамическим массивом структур журналов
+	// Р Р°Р±РѕС‚Р° СЃ РґРёРЅР°РјРёС‡РµСЃРєРёРј РјР°СЃСЃРёРІРѕРј СЃС‚СЂСѓРєС‚СѓСЂ Р¶СѓСЂРЅР°Р»РѕРІ
 	for (int i = 0; i < 4; i++) {
 		createMagazine(magazines_dynamic + i);
 	}
